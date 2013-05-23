88b94941e5524edd0738b171e0c9aa3b976be042
Phil Leggetter <phil@leggetter.co.uk>
2013-05-23 22:43:30 +0100

    additional padding when scrolling

--- js/behaviours.js
+++ js/behaviours.js
@@ -1,6 +1,6 @@
 $('a').click(function() {
     var destHref = $( this ).attr( 'href' ).replace( '#', '' );
-    var destination = $( 'a[name=' + destHref + ']' ).offset().top - 130; // TODO: -130 should be calculated
+    var destination = $( 'a[name=' + destHref + ']' ).offset().top - 150; // TODO: -150 should be calculated
     $( 'html:not(:animated),body:not(:animated)' ).animate( { scrollTop: destination }, 500 );
     return false;
 });

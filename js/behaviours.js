9e578987da793fb6aff1e8bf592c100dd24f57b5
Phil Leggetter <phil@leggetter.co.uk>
2013-10-03 19:15:50 +0100

    fixing page title and highlighting current/active nav item

--- js/behaviours.js
+++ js/behaviours.js
@@ -1,7 +1,15 @@
 $( function() {
 	var url = document.location.pathname;
 	var links = $( 'a[href="' + url + '"]' );
-	links.addClass( 'active' );
+
+  var parent;
+	links.each( function( i, el ) {
+    el = $( el );
+    parent = el.parent();
+    if( parent.get( 0 ).tagName.toLowerCase() === 'li' ) {
+      parent.addClass( 'active' );
+    }
+  } );
 
 	hljs.initHighlightingOnLoad();
 } );

e4203c4619041834d6c4486731dd365ddd57cfa6
Phil Leggetter <phil@leggetter.co.uk>
2013-10-30 12:17:26 +0000

    image optimisation

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    
+    imagemin: {                          // Task
+      docImages: {                         // Another target
+        options: {                       // Target options
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'docs/',                  // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'docs/'                  // Destination path prefix
+        }]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['imagemin']);
+
+};

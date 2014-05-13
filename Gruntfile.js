57e0a945e3461905c51bf59a141d93689b99cc2e
leggetter <phil@leggetter.co.uk>
2014-05-13 10:49:09 +0100

    optimizing images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,9 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    
-    imagemin: {                          // Task
+
+    imagemin: {
+                               // Task
       docImages: {                         // Another target
         options: {                       // Target options
           optimizationLevel: 3
@@ -13,7 +14,20 @@ module.exports = function(grunt) {
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
           dest: 'docs/'                  // Destination path prefix
         }]
+      },
+
+      blogImages: {                         // Another target
+        options: {                       // Target options
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'blog/',                  // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'blog/'                  // Destination path prefix
+        }]
       }
+
     }
 
   });

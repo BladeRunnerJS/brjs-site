9365a5967e258a36d7d4cc57a1722cda6702293d
dchambers <dominic.chambers@gmail.com>
2014-11-26 12:25:59 +0000

    Stop using the tocPrefix now that we're using the heading text, as this makes the links more pleasing to the eye.

--- js/jquery.toc.js
+++ js/jquery.toc.js
@@ -23,7 +23,7 @@
       else {
         // TODO: replace this work-around with a proper solution once <https://github.com/dafi/tocmd-generator/issues/1> is fixed
 //        anchorId = config.anchorPrefix + tocLevel + '-' + tocSection;
-        anchorId = encodeURIComponent(config.anchorPrefix + el.text().toLowerCase().replace(/ /g, '-'));
+        anchorId = encodeURIComponent(el.text().toLowerCase().replace(/ /g, '-'));
         if( innerSection ) {
           anchorId += '-' + innerSection;
         }
